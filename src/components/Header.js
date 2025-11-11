24da5aef595cdeef0bb41cf23d730be7bd434638
abi4679553 <abivengadajalam7708@gmail.com>
2025-11-11 21:51:39 +0530

    header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <p className='bg-red-500 text-center text-white p-5 text-2xl'>Hello !!</p>
+    </div>
+  )
+}
+
+export default Header
