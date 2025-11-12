da9ec99ba72619514adf5aa4cf9672ade9686e8d
abi4679553 <abivengadajalam7708@gmail.com>
2025-11-12 06:04:19 +0530

    home

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-        <p className='bg-red-500 text-center text-white p-5 text-2xl'>Hello !!</p>
+    <div className='flex  justify-between gap-5 px-10 bg-blue-500  items-center text-xl text-white'>
+        <p className=' p-5 text-2xl'>Hello !!</p>
+        <p>Home</p>
+        <p>About</p>
+        <p>Contact</p>
+        
     </div>
   )
 }
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import img  from "../assests/images.jpeg"
+
+const Home = () => {
+  return (
+    <div className='p-5 ml-[35%]'>
+        <img src={img} alt='image' className='h-[300px] w-[300px] items-center  ' />
+        <p> ...jejgor;q3kdjfgwakrefhnlwhr4jt</p>
+
+    </div>
+  )
+}
+
+export default Home
