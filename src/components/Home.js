1d0def0406948ba79679df70d24cfb107f70370f
abi4679553 <abivengadajalam7708@gmail.com>
2025-11-14 21:04:40 +0530

    img

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import img  from "../assests/Screenshot 2025-09-29 212123.png"
 
 const Home = () => {
   return (
     <div>
        
         <p>al,qmdlwjkwj</p>
+         <img src={img} alt='image' className='h-[400px] w-full items-center  ' />
     </div>
   )
 }
